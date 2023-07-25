3821859363eb34caf99683d610f428de00e0b71f
Hervé Seitz <herve.seitz@igh.cnrs.fr>
2023-07-25 12:02:43 +0200

    Add files via upload

---
+++ Figure_4/Script_spike-in_count.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sample=$1
+
+echo "Strategy wt_spike_count m2_spike_count m6_spike_count" > Spike-in_counts_sample_$sample'.txt'
+wt=`grep -c '^TGAGATCATTCGCAAAGCTGATT$' adapter-trimmed_$sample'.fastq'`
+m2=`grep -c '^TCAAAATATTCGCAAAGCTGATT$' adapter-trimmed_$sample'.fastq'`
+m6=`grep -c '^TCCACTAATTCGCAAAGCTGATT$' adapter-trimmed_$sample'.fastq'`
+echo "Grep_full-length "$wt $m2 $m6
+wt=`grep -c 'GATCATTCGCAAAGCTG' adapter-trimmed_$sample'.fastq'`
+m2=`grep -c 'AAATATTCGCAAAGCTG' adapter-trimmed_$sample'.fastq'`
+m6=`grep -c 'ACTAATTCGCAAAGCTG' adapter-trimmed_$sample'.fastq'`
+echo "Grep_with_isoforms "$wt $m2 $m6
